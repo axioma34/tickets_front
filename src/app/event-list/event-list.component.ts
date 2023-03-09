6b514fbc7840cb32c2dcf805441bc855d64c94d1
andreirennit <andrew.rennit@gmail.com>
2023-03-09 14:39:16 +0700

    Error handler added to display if server error occurred + vscode config removed

--- src/app/event-list/event-list.component.ts
+++ src/app/event-list/event-list.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core'
 import {HttpService} from "../services/http.service"
 import {Event} from "../event"
+import {MatSnackBar} from "@angular/material/snack-bar"
+
 @Component({
   selector: 'event-list',
   templateUrl: './event-list.component.html',
@@ -11,9 +13,19 @@ export class EventListComponent implements OnInit {
   title = 'ticketFront'
   events: Event[] = []
 
-  constructor(private httpService: HttpService){}
+  constructor(private httpService: HttpService, private alert: MatSnackBar) {
+  }
 
-  ngOnInit(){
-    this.httpService.getData().subscribe({next:(data:any) => this.events = data})
+  ngOnInit() {
+    this.httpService.getData().subscribe({
+        next: (data: any) => {
+          this.events = data
+        },
+        error: error => {
+          console.log(error)
+          this.alert.open('Server error occurred!','X', {duration: 5000, panelClass: ['error-alert']})
+        }
+      }
+    )
   }
 }
